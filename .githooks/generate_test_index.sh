d4b6c3aa1ba6f742ae3911f556333de839e2a89e
ESapenaVentura <sapenaventura@gmail.com>
2020-05-14 15:25:34 +0100

    Script will only run when adoc files are modified

--- .githooks/generate_test_index.sh
+++ .githooks/generate_test_index.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Generate graph_test_set/README.md document with updated tests and add it to commit.
+if git diff --name-only --cached | grep -Eq '*.adoc$'; then
+  python3 .readme/update_test_index.py
+  git add graph_test_set/README.md
+fi
 
-python3 .readme/update_test_index.py
-git add graph_test_set/README.md
